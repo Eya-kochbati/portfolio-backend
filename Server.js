1cdfe416d35bd7eabc9f3a3ea7ec5fc654688a51
Eya-kochbati <86416190+Eya-kochbati@users.noreply.github.com>
2026-09-17 18:16:23 +0100

    server creation

---
+++ Server.js
@@ -0,0 +1,19 @@
+//requirements
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (err) => {
+    err? console.log(err) :
+    console.log(`Server is running on port ${PORT}`);
+});
